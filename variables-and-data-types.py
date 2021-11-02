8d3fa17cdfed6b654a1cd5a7e7747f1adb26b647
dani.9131 <you@example.com>
2021-11-02 17:47:33 -0500

    update

---
+++ variables-and-data-types.py
@@ -0,0 +1,76 @@
+print("Hello World")
+
+my_text="Hello World"
+print(my_text)
+
+print("Hello World, Goodbye World")
+
+glass_of_water=1
+print("I drank", glass_of_water, "glasses of water today.")
+
+glass_of_water=3
+glass_of_water=glass_of_water + 1
+print("I drank", glass_of_water,"glasses of water today")
+
+men_stepped_on_the_moon=1
+print("Individuals", men_stepped_on_the_moon, "stepped on the moon")
+
+String1= "My reason for coding is to make change"
+print(String1)
+print(type(String1))
+
+global_mean_sea_level_2018=21
+float(global_mean_sea_level_2018)
+print(float(global_mean_sea_level_2018))
+
+staying_alive= "False"
+print(bool(staying_alive))
+
+men_stepped_on_the_moon=12
+answer_1=men_stepped_on_the_moon
+print(type(answer_1))
+
+my_reason_for_coding="intergalactic travel"
+answer_2=my_reason_for_coding
+print(type(answer_2))
+
+global_mean_sea_level_delta_2018=21.36
+answer_3=global_mean_sea_level_delta_2018
+print(type(answer_3))
+
+staying_alive=True
+answer_4=staying_alive
+print(type(answer_4))
+
+my_grade="10"
+answer_5=int (my_grade)
+print(type(answer_5))
+
+my_temp=97.70
+answer_6=int (my_temp)
+print(type(answer_6))
+
+shoe_price="69.99"
+answer_7=float(shoe_price)
+print(type(answer_7))
+
+gross_world_product=84.84
+gwp_str=str(gross_world_product)
+answer_8="In 2018 gross product of the world (GWP) was " + gwp_str + " in trillion US dollars."
+print(answer_8)
+print(type(answer_8))
+
+gift_list=[]
+answer_9=gift_list
+print(type(answer_9))
+
+bucket_list= ()
+answer_11=bucket_list
+print(type(answer_11))
+
+gift_list=["strawberry", "bananas", "apples"]
+print(gift_list)
+print(type(gift_list))
+
+
+
